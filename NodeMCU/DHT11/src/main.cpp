b192261323ae0e7da5582fb5c2867c46b16b75f0
unknown <akshaykalyan>
2019-06-19 17:15:41 +0530

    DHT11 Library added

--- NodeMCU/DHT11/src/main.cpp
+++ NodeMCU/DHT11/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#include "DHT.h"        // including the library of DHT11 temperature and humidity sensor
+#define DHTTYPE DHT11   // DHT 11
 
+#define dht_dpin 0
+DHT dht(dht_dpin, DHT11); 
+void setup(void)
+{ 
+  dht.begin();
+  Serial.begin(9600);
+  Serial.println("Humidity and temperature\n\n");
+  delay(700);
+
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();         
+    Serial.print("Current humidity = ");
+    Serial.print(h);
+    Serial.print("%  ");
+    Serial.print("temperature = ");
+    Serial.print(t); 
+    Serial.println("C  ");
+  delay(800);
 }
